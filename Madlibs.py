cda76f191c5b52ebfe88841b20654aa7cb35e7c6
TheSK <thesk@Mac.lan>
2026-01-08 18:31:14 -0600

    Gamer1

---
+++ Madlibs.py
@@ -0,0 +1,15 @@
+#YouMEverse
+
+
+first = input("Enter your name as SAM: ")
+second = input("Enter you babs as AJ: ")
+print("Guess the right words and get a Momo treat")
+
+adjective1 = input("Enter an adjective of something u make the best: ")
+noun = input("Enter a noun of what AJ made today:  ")
+verb = input("Enter a verb '-ing' of what AJ does everyday to Sam: ")
+
+print(f"Today SAM made AJ a {adjective1} chiya.")
+print(f"AJ cooked {noun}.")
+print(f"AJ is always {verb} SAM with Dhurandar dance.")
+print(f"AJ is the blessed part of SAM.")
